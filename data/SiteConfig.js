6263d341d8f2184af24d65a6a1476cd9a288586d
Dani Lucaci <danilucaci@gmail.com>
2018-07-18 21:50:02 +0200

    Add updated icons and logo

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   siteTitle: "danilucaci.com UX/UI Designer", // Site title.
   siteTitleShort: "danilucaci.com", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "danilucaci.com Personal Blog", // Alternative site title for SEO.
-  siteLogo: "/icons/logo.svg", // Logo used for SEO and manifest.
+  siteLogo: "/svg/logo.svg", // Logo used for SEO and manifest.
   siteUrl: "https://www.danilucaci.com", // Domain of your website without pathPrefix.
   pathPrefix: "/danilucaci", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "danilucaci.com Personal Blog.", // Website description used for RSS feeds/meta description tag.
@@ -18,24 +18,6 @@ module.exports = {
   userLocation: "North Pole, Earth", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription: "I am a UX/UI Designer based in Barcelona.", // User description to display in the author segment.
-  // Links to social profiles/projects you want to display in the author segment/navigation bar.
-  userLinks: [
-    {
-      label: "GitHub",
-      url: "https://github.com/danilucaci",
-      iconClassName: "fa fa-github",
-    },
-    {
-      label: "Twitter",
-      url: "https://twitter.com/danilucaci",
-      iconClassName: "fa fa-twitter",
-    },
-    {
-      label: "Email",
-      url: "mailto:danilucaci@gmail.com",
-      iconClassName: "fa fa-envelope",
-    },
-  ],
   copyright: "&copy;2018 Copyright Dani Lucaci. <br /> All rights reserved.",
   builtWith: "This site is built with GatsbyJS and hosted on Netlify.",
   themeColor: "#c62828",
