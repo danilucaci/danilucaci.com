f97f766b20141ccf40e760f0c6902c7f8153f366
Dani Lucaci <danilucaci@gmail.com>
2018-07-18 23:48:59 +0200

    Refactor to rem function and remove mixin prefix

--- src/components/SiteHeader/MenuButton/MenuButton.js
+++ src/components/SiteHeader/MenuButton/MenuButton.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-// import IconMenu from "../../../../static/icons/sass.svg";
-
 const MenuButton = () => {
   return (
     <button className="a-menu__btn">
       <span className="copy copy--s copy--b copy--menu">Menu</span>
       <div className="a-menu__icon" aria-hidden="true">
-        {/* <IconMenu className="icon icon--menu" /> */}
+        <img src="./menu.svg" alt="" />
       </div>
     </button>
   );
