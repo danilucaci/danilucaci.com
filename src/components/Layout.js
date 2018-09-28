ab0b47264f409d099e3664e537b8727a4679ad50
Dani Lucaci <danilucaci@gmail.com>
2018-09-28 15:25:02 +0200

    Remove html.js, move twitter js to Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,6 +10,7 @@ import GlobalFonts from "../theme/globalFonts";
 import GlobalReset from "../theme/globalReset";
 import GlobalAria from "../theme/globalAria";
 import GlobalHTML from "../theme/globalCSS";
+import { SVGSprite } from "./SVGSprite/SVGSprite";
 
 import { ScrollProvider } from "./ScrollProvider/ScrollProvider";
 
@@ -83,7 +84,26 @@ class Layout extends Component {
           <Helmet>
             <title>{config.siteTitle}</title>
             <meta name="description" content={config.siteDescription} />
+            {/* Twitter */}
+            <script>
+              {`window.twttr = (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0],
+                  t = window.twttr || {};
+                if (d.getElementById(id)) return t;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js, fjs);
+
+                t._e = [];
+                t.ready = function(f) {
+                  t._e.push(f);
+                };
+                return t;
+              }(document, "script", "twitter-wjs"));`}
+            </script>
           </Helmet>
+          <SVGSprite />
           <ScrollProvider location={this.props.location}>
             {this.props.children}
           </ScrollProvider>
