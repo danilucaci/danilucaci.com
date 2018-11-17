cf752c6c02d3a54baf375f8f878e2a0d560e2ac4
Dani Lucaci <danilucaci@gmail.com>
2018-11-17 12:21:31 +0100

    Add list style component

--- src/components/Copy/Copy.js
+++ src/components/Copy/Copy.js
@@ -38,3 +38,30 @@ export const CopyItalic = styled(Copy)`
   font-weight: 400;
   font-style: italic;
 `;
+
+export const BulletList = styled.ul`
+  color: ${theme.colors.dark800};
+  display: block;
+  list-style-type: disc;
+  list-style-position: inside;
+  margin: 0;
+  padding: 0;
+
+  .fonts-loaded & {
+    font-family: ${theme.fonts.bodyRegular};
+  }
+
+  font-weight: ${(props) => (props.bold ? 700 : 400)};
+  font-style: normal;
+
+  font-size: ${(props) =>
+    props.small ? props.theme.fontSizes.s : props.theme.fontSizes.m};
+
+  line-height: ${(props) =>
+    props.small ? props.theme.lineHeights.s : props.theme.lineHeights.m};
+
+  letter-spacing: ${(props) =>
+    props.small ? props.theme.letterSpacing.s : props.theme.letterSpacing.m};
+`;
+
+export const BulletListItem = styled.li``;
