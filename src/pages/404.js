870fe7d1a4003ccc2c9c2ec2c15f49c12bd41f3c
Dani Lucaci <danilucaci@gmail.com>
2019-01-27 22:44:37 +0100

    Add lang props in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,20 +7,26 @@ import { Main } from "../components/Main/Main";
 import SiteFooter from "../components/SiteFooter/SiteFooter";
 import intlMessages from "../i18n/i18n";
 
-const NotFoundPage = (props) => (
-  <Layout location={props.location} locale={props.pageContext.locale}>
-    <Helmet
-      title={`${
-        intlMessages[props.pageContext.locale].meta.pageNotFoundMetaTitle
-      }`}
-    />
-    <SEO />
-    <SiteHeader />
-    <Main role="main">
-      <h1>404 de mine</h1>
-    </Main>
-    <SiteFooter />
-  </Layout>
-);
+const NotFoundPage = (props) => {
+  let lang = props.pageContext.locale;
+  let changeLanguage = "";
+
+  if (lang === "en") {
+    changeLanguage = "/es/404";
+  } else if (lang === "es") {
+    changeLanguage = "/404";
+  }
+  return (
+    <Layout location={props.location} locale={lang}>
+      <Helmet title={`${intlMessages[lang].meta.pageNotFoundMetaTitle}`} />
+      <SEO />
+      <SiteHeader locale={lang} />
+      <Main role="main">
+        <h1>404 de mine</h1>
+      </Main>
+      <SiteFooter changeLanguage={changeLanguage} />
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
