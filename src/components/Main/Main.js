874a75e17bc1e70134113207de49edb45aeccb54
Dani Lucaci <danilucaci@gmail.com>
2019-02-03 21:43:48 +0100

    Make page header fixed on mobile

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 import { theme, rem, mediaMin } from "../../theme/globalStyles";
 
 export const Main = styled.main`
-  margin-top: ${theme.mainMargins.top.s};
+  margin-top: calc(${theme.navBarHeight} + ${theme.mainMargins.top.s});
   margin-bottom: ${theme.mainMargins.bottom.s};
 
   ${mediaMin.m`
   /* Add in extra space for the fixed page navbar */
-  margin-top: calc(${theme.navBarHeight} + ${theme.mainMargins.top.m});
-  margin-bottom: ${theme.mainMargins.bottom.m};
+    margin-top: calc(${theme.navBarHeight} + ${theme.mainMargins.top.m});
+    margin-bottom: ${theme.mainMargins.bottom.m};
   `};
 
   ${mediaMin.xl`
