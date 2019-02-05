4313290e951f164fd6fb365fe311f17ec8adb5e7
Dani Lucaci <danilucaci@gmail.com>
2019-02-05 15:37:02 +0100

    Remove bottom margin from Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,17 +3,14 @@ import { theme, rem, mediaMin } from "../../theme/globalStyles";
 
 export const Main = styled.main`
   margin-top: calc(${theme.navBarHeight} + ${theme.mainMargins.top.s});
-  margin-bottom: ${theme.mainMargins.bottom.s};
 
   ${mediaMin.m`
   /* Add in extra space for the fixed page navbar */
     margin-top: calc(${theme.navBarHeight} + ${theme.mainMargins.top.m});
-    margin-bottom: ${theme.mainMargins.bottom.m};
   `};
 
   ${mediaMin.xl`
   /* Add in extra space for the fixed page navbar */
     margin-top: calc(${theme.navBarHeight} + ${theme.mainMargins.top.xl});
-    margin-bottom: ${theme.mainMargins.bottom.xl};
   `};
 `;
