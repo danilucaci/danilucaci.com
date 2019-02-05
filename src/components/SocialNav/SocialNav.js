55699421581a0e0a51d66231194a107ba57b4de4
Dani Lucaci <danilucaci@gmail.com>
2019-02-05 21:32:34 +0100

    Make SocialNav icons adjust size with viewport

--- src/components/SocialNav/SocialNav.js
+++ src/components/SocialNav/SocialNav.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import styled, { css } from "styled-components";
-import { theme, rem, mediaMin } from "../../theme/globalStyles";
+import { theme, rem, mediaMax, mediaMin } from "../../theme/globalStyles";
 
 import { Icon } from "../Icon/Icon";
 
@@ -9,6 +9,13 @@ const Wrapper = styled.div``;
 
 const StyledIcon = styled(Icon)`
   fill: ${(props) => (props.light ? theme.colors.light100 : null)};
+  width: ${rem(40)};
+  height: ${rem(40)};
+
+  ${mediaMin.m`
+    width: ${rem(32)};
+    height: ${rem(32)};  
+  `};
 `;
 
 const StyledLink = styled.a`
@@ -17,6 +24,10 @@ const StyledLink = styled.a`
   margin-right: ${rem(16)};
   will-change: transform;
   transition: transform ease 0.15s;
+
+  ${mediaMin.m`
+    margin-right: ${rem(10)};
+  `};
 
   &:last-of-type {
     margin-right: 0;
@@ -40,7 +51,7 @@ const SocialNav = (props) => {
         <span className="sr-only">
           View danilucaci Github account, opens in new window
         </span>
-        <StyledIcon size="40" aria-hidden="true" light={props.light}>
+        <StyledIcon aria-hidden="true" light={props.light}>
           <use xlinkHref="#github" />
         </StyledIcon>
       </StyledLink>
@@ -52,7 +63,7 @@ const SocialNav = (props) => {
         <span className="sr-only">
           View danilucaci Twitter account, opens in new window
         </span>
-        <StyledIcon size="40" aria-hidden="true" light={props.light}>
+        <StyledIcon aria-hidden="true" light={props.light}>
           <use xlinkHref="#twitter" />
         </StyledIcon>
       </StyledLink>
@@ -64,7 +75,7 @@ const SocialNav = (props) => {
         <span className="sr-only">
           View danilucaci Drible account, opens in new window
         </span>
-        <StyledIcon size="40" aria-hidden="true" light={props.light}>
+        <StyledIcon aria-hidden="true" light={props.light}>
           <use xlinkHref="#dribbble" />
         </StyledIcon>
       </StyledLink>
@@ -76,7 +87,7 @@ const SocialNav = (props) => {
         <span className="sr-only">
           View danilucaci Linkedin account, opens in new window
         </span>
-        <StyledIcon size="40" aria-hidden="true" light={props.light}>
+        <StyledIcon aria-hidden="true" light={props.light}>
           <use xlinkHref="#linkedin" />
         </StyledIcon>
       </StyledLink>
