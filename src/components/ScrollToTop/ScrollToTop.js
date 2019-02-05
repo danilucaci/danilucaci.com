a529086f26a8ea95f07f97bdbd826748edf04279
Dani Lucaci <danilucaci@gmail.com>
2019-02-05 18:53:42 +0100

    Make ScrollToTop smaller

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -5,16 +5,16 @@ import { theme, rem, mediaMin } from "../../theme/globalStyles";
 
 const StyledScrollToTop = styled.a`
   background-color: ${theme.colors.gray300};
-  opacity: 0.88;
+  opacity: 0.92;
   text-decoration: none;
   position: fixed;
-  bottom: ${rem(24)};
-  right: ${rem(24)};
+  bottom: ${rem(16)};
+  right: ${rem(16)};
   display: block;
 
   ${theme.shadow.default};
 
-  border-radius: ${rem(4)};
+  border-radius: ${theme.borderRadius.buttons};
   will-change: transform;
 
   &:hover {
@@ -25,8 +25,8 @@ const StyledScrollToTop = styled.a`
 `;
 
 const ScrollToTopIcon = styled(Icon)`
-  width: ${rem(48)};
-  height: ${rem(48)};
+  width: ${rem(40)};
+  height: ${rem(40)};
   padding: ${rem(8)};
 `;
 
