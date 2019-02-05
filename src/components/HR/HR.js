e7e394e6e30a0d9ae345ca60f0a0d3ada9b08cf4
Dani Lucaci <danilucaci@gmail.com>
2019-02-05 23:11:01 +0100

    Made HR.js a span tag to be ignored by screen readers

--- src/components/HR/HR.js
+++ src/components/HR/HR.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { theme, mediaMin, rem, mediaMax } from "../../theme/globalStyles";
 
-export const HR = styled.hr`
-  border: 1px solid ${theme.colors.sectionBackground};
+export const HR = styled.span`
+  display: block;
+  border: 2px solid ${theme.colors.sectionBackground};
 `;
