d4b74cb89124d6e9540896ea965fc8c45c63bf4f
Dani Lucaci <danilucaci@gmail.com>
2019-03-24 19:49:10 +0100

    Reorder gatsby-config plugins, add gatsby-remark-external-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const urljoin = require("url-join");
+// const urljoin = require("url-join");
 const config = require("./data/SiteConfig");
 
 module.exports = {
@@ -19,31 +19,6 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-catch-links",
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        output: "/sitemap.xml",
-        // Exclude specific pages or groups of pages using glob parameters
-        // See: https://github.com/isaacs/minimatch
-        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-        // exclude: ["/es/404"],
-        query: `
-          {
-            site {
-              siteMetadata {
-                siteUrl
-              }
-            }
-            allSitePage {
-              edges {
-                node {
-                  path
-                }
-              }
-            }
-        }`,
-      },
-    },
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
@@ -93,6 +68,116 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    // {
+    //   resolve: "gatsby-mdx",
+    //   options: {
+    //     extensions: [".mdx", ".md"],
+    //     gatsbyRemarkPlugins: [
+    //       {
+    //         resolve: "gatsby-remark-copy-linked-files",
+    //       },
+    //       {
+    //         resolve: "gatsby-remark-images",
+    //         options: {
+    //           maxWidth: 1128,
+    //           linkImagesToOriginal: false,
+    //           withWebp: true,
+    //           backgroundColor: "transparent",
+    //           showCaptions: true,
+    //         },
+    //       },
+    //       {
+    //         resolve: "gatsby-remark-autolink-headers",
+    //         options: {
+    //           offsetY: "0",
+    //           icon:
+    //             "<svg aria-hidden=\"true\" height=\"24\" width=\"24\" version=\"1.1\" viewBox=\"0 0 16 16\"><path fill-rule=\"evenodd\" d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg>",
+    //           className: "headings-anchor",
+    //         },
+    //       },
+    //       {
+    //         resolve: "gatsby-remark-external-links",
+    //         options: {
+    //           target: "_blank",
+    //           rel: "nofollow noopener noreferrer",
+    //         },
+    //       },
+    //       {
+    //         resolve: "gatsby-remark-prismjs",
+    //         options: {
+    //           classPrefix: "language-",
+    //         },
+    //       },
+    //       // TODO: Replace with "mdx-component-autolink-headers"
+    //     ],
+    //   },
+    // },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1128,
+              linkImagesToOriginal: false,
+              withWebp: true,
+              backgroundColor: "transparent",
+              showCaptions: true,
+            },
+          },
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            options: {
+              offsetY: "0",
+              icon:
+                "<svg aria-hidden=\"true\" height=\"24\" width=\"24\" version=\"1.1\" viewBox=\"0 0 16 16\"><path fill-rule=\"evenodd\" d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg>",
+              className: "headings-anchor",
+            },
+          },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow noopener noreferrer",
+            },
+          },
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              classPrefix: "language-",
+            },
+          },
+          "gatsby-remark-copy-linked-files",
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        output: "/sitemap.xml",
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page`
+        // and all routes beginning with `category`
+        // exclude: ["/es/404"],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`,
+      },
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
@@ -112,44 +197,5 @@ module.exports = {
       },
     },
     "gatsby-plugin-offline",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              // https://www.gatsbyjs.org/packages/gatsby-remark-images/?=images
-              maxWidth: 1128,
-              linkImagesToOriginal: false,
-              withWebp: true,
-              // wrapperStyle: 	Add custom styles to the div wrapping the responsive images.
-              // Use the syntax for the style attribute e.g. margin-bottom:10px; background: red;
-              // wrapperStyle: "padding: 24px; background-color: red;",
-              // backgroundColor: Set the background color of the image to match
-              // the background image of your design.
-              // default: white
-              backgroundColor: "transparent",
-              // showCaptions: Add a caption to each image with the contents of the title attribute,
-              // when this is not empty. Set this option to true to enable this behavior.
-              // when on, turns everything in a figure with figcaption
-              showCaptions: true,
-            },
-          },
-          {
-            resolve: "gatsby-remark-autolink-headers",
-            options: {
-              offsetY: "0",
-              icon:
-                "<svg aria-hidden=\"true\" height=\"24\" width=\"24\" version=\"1.1\" viewBox=\"0 0 16 16\"><path fill-rule=\"evenodd\" d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg>",
-              className: "headings-anchor",
-            },
-          },
-          "gatsby-remark-responsive-iframe",
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-        ],
-      },
-    },
   ],
 };
