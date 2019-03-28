59069e5869635557fcbf1b0dad23ae9be5c5006b
Dani Lucaci <danilucaci@gmail.com>
2019-03-28 16:29:57 +0100

    Move about me social nav bellow copy

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -65,37 +65,7 @@ const HeaderInfo = styled.header`
   }
 `;
 
-const StyledSocialNav = styled.div`
-  margin-top: ${rem(24)};
-  max-width: ${theme.contain.wrapper.col10};
-
-  padding-right: ${theme.gutters.s};
-  padding-left: ${theme.gutters.s};
-
-  /* iPhone X */
-  @supports (padding: max(0px)) {
-    & {
-      padding-left: max(${theme.gutters.s}, env(safe-area-inset-left));
-      padding-right: max(${theme.gutters.s}, env(safe-area-inset-right));
-    }
-  }
-
-  ${mediaMin.s`
-    padding-right: ${theme.gutters.m};
-    padding-left: ${theme.gutters.m};
-
-    /* iPhone X */
-    @supports (padding: max(0px)) {
-      & {
-        padding-left: max(${theme.gutters.m}, env(safe-area-inset-left));
-        padding-right: max(${theme.gutters.m}, env(safe-area-inset-right));
-      }
-    }
-  `};
-
-  margin-left: auto;
-  margin-right: auto;
-`;
+const StyledSocialNav = styled.div``;
 
 const ImageWrapper = styled.div`
   background: ${theme.colors.sectionBackground};
@@ -442,11 +412,12 @@ const AboutPage = (props) => {
               <FormattedMessage id="aboutMeCopy3">
                 {(txt) => <AboutCopy>{txt}</AboutCopy>}
               </FormattedMessage>
+              <StyledSocialNav>
+                <SocialNav />
+              </StyledSocialNav>
             </InfoWrapper>
           </HeaderInfo>
-          <StyledSocialNav>
-            <SocialNav />
-          </StyledSocialNav>
+
           <MoreAboutMeWrapper>
             <MoreAboutMeInner>
               <FormattedMessage id="aboutMeWhatNowTitle">
