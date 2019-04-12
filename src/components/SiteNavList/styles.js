dff6aea370a5e035848682a06b48a252ddeed703
Dani Lucaci <danilucaci@gmail.com>
2019-04-12 10:16:54 +0200

    SiteNavList code cleanup

--- src/components/SiteNavList/styles.js
+++ src/components/SiteNavList/styles.js
@@ -4,7 +4,7 @@ import { theme, rem, mediaMin } from "../../theme/globalStyles";
 export const StyledSiteNavList = styled.ul`
   background-color: ${theme.colors.grey100};
   text-align: center;
-
+  display: inline-block;
   padding-left: ${rem(16)};
   padding-right: ${rem(16)};
 
@@ -20,22 +20,19 @@ export const StyledSiteNavList = styled.ul`
   left: 0;
   top: calc(${theme.navBarHeight} - ${rem(8)});
   transform: translateX(120%);
-  transition: transform ease 0.35s;
+  transition: transform ease 0.30s;
 
   ${(props) =>
     props.showNav &&
     css`
       transform: translateX(0);
-      visibility: visible;
-      opacity: 1;
-      pointer-events: auto;
     `};
-
+/* 
   ${(props) =>
     props.isTransitioning &&
     `
       display: block !important;
-    `};
+    `}; */
 
   ${mediaMin.xs`
     background-color: transparent;
@@ -47,7 +44,5 @@ export const StyledSiteNavList = styled.ul`
     width: auto;
     position: static;
     transform: none;
-    visibility: visible;
-    pointer-events: auto;
   `};
 `;
