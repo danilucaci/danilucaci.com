e818c4bddbbff2c697217d385053911cf560a35c
Dani Lucaci <danilucaci@gmail.com>
2019-04-19 16:15:15 +0200

    Display current language always with en/es

--- src/components/LanguageSelector/styles.js
+++ src/components/LanguageSelector/styles.js
@@ -68,17 +68,6 @@ export const StyledLanguageDropdown = styled.ul`
 export const LanguageDropdownLabel = styled.span`
   display: inline-block;
   vertical-align: middle;
-
-  ${mediaMin.xs`
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    border: 0;
-  `};
 `;
 
 export const CurrentLanguageIcon = styled(Icon)`
