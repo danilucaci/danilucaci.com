70302eab07306c4dd4599dda4e16f22d22a98a05
Dani Lucaci <danilucaci@gmail.com>
2019-04-19 09:06:35 +0200

    Restore SiteNav iPhoneX padding

--- src/components/SiteNav/styles.js
+++ src/components/SiteNav/styles.js
@@ -11,20 +11,11 @@ export const StyledSiteNav = styled.nav`
   padding-right: ${theme.gutters.s};
   padding-left: ${theme.gutters.s};
 
-  /* iPhone X 
-   * Add the extra gridSpacing used in the Grid
-   * calc(env(safe-area-inset-left) + ${theme.gridSpacing.s})
-   */
+  /* iPhone X */
   @supports (padding: max(0px)) {
     & {
-      padding-left: max(
-        ${theme.gutters.s},
-        calc(env(safe-area-inset-left) + ${theme.gridSpacing.s})
-      );
-      padding-right: max(
-        ${theme.gutters.s},
-        calc(env(safe-area-inset-left) + ${theme.gridSpacing.s})
-      );
+      padding-left: max(${theme.gutters.s}, env(safe-area-inset-left));
+      padding-right: max(${theme.gutters.s}, env(safe-area-inset-right));
     }
   }
 
@@ -35,12 +26,8 @@ export const StyledSiteNav = styled.nav`
     /* iPhone X */
     @supports (padding: max(0px)) {
       & {
-        padding-left: max(${theme.gutters.m}, calc(env(safe-area-inset-left) + ${
-  theme.gridSpacing.m
-}));
-        padding-right: max(${theme.gutters.m}, calc(env(safe-area-inset-left) + ${
-  theme.gridSpacing.m
-}));
+        padding-left: max(${theme.gutters.m}, env(safe-area-inset-left));
+        padding-right: max(${theme.gutters.m}, env(safe-area-inset-right));
       }
     }
   `};
