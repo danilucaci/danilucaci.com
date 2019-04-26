5ed2ec50242ccb32c36f40dfae92bc8430bd1c19
Dani Lucaci <danilucaci@gmail.com>
2019-04-26 11:01:33 +0200

    Destructure MCLoadingCTA propTypes

--- src/components/MCLoadingCTA/MCLoadingCTA.js
+++ src/components/MCLoadingCTA/MCLoadingCTA.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { bool, string } from "prop-types";
 
 import Spinner from "../Spinner/Spinner";
 import { FORM_SUBMIT_STATUS } from "../../i18n/i18n";
@@ -9,7 +9,7 @@ import { StyledLoadingCTA } from "./styles";
 function MCLoadingCTA({ showMCError = false, showMCLoading = true, locale = "en" }) {
   return (
     <StyledLoadingCTA error={showMCError}>
-      {showMCLoading && <Spinner locale={locale} />}
+      {showMCLoading && <Spinner />}
       {!showMCLoading && !showMCError && <>{FORM_SUBMIT_STATUS.subscribeCta[locale]}</>}
       {!showMCLoading && showMCError && <>{FORM_SUBMIT_STATUS.ctaError[locale]}</>}
     </StyledLoadingCTA>
@@ -17,9 +17,13 @@ function MCLoadingCTA({ showMCError = false, showMCLoading = true, locale = "en"
 }
 
 MCLoadingCTA.propTypes = {
-  showMCLoading: PropTypes.bool.isRequired,
-  showMCError: PropTypes.bool,
-  locale: PropTypes.string.isRequired,
+  showMCLoading: bool.isRequired,
+  showMCError: bool,
+  locale: string.isRequired,
+};
+
+MCLoadingCTA.defaultProps = {
+  showMCError: false,
 };
 
 export default MCLoadingCTA;
