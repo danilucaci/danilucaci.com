c93a1acb11df8ec09bc2f3877f69f05dc68e59a1
Dani Lucaci <danilucaci@gmail.com>
2019-05-23 22:29:29 +0200

    Add alt for Dribbble Images

--- src/components/DribbblePost/DribbblePost.js
+++ src/components/DribbblePost/DribbblePost.js
@@ -44,7 +44,7 @@ function DribbblePost(props) {
   }
 
   return (
-    <StyledLink href={htmlURL} target="_blank" rel="noopener noreferrer">
+    <StyledLink href={htmlURL} target="_blank" rel="noopener noreferrer" alt={title}>
       <StyledDribbblePost>
         <PostBGImg>
           <StyledImg
