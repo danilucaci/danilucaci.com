c21a9746bfb85079ae95a2bd3f591b51f99a6355
Dani Lucaci <danilucaci@gmail.com>
2019-08-31 12:49:55 +0200

    Clean up globalAria.js

--- src/theme/globalAria.js
+++ src/theme/globalAria.js
@@ -1,19 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import { theme } from "./theme";
 
 const GlobalAria = createGlobalStyle`
-  ${
-    "" /* button,
-  a,
-  input {
-    &:active,
-    &:focus {
-      outline: 2px solid ${theme.colors.grey800};
-      ${theme.shadow.input.focus};
-    }
-  } */
-  }
-
   /* Browser fallback for non-supporting hidden attribute
   * Usefull in ul lists to hide the ul element
   */
