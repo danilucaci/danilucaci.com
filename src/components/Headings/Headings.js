3aa45237df7c6ae87291714f0da2a4a6415cd26d
Dani Lucaci <danilucaci@gmail.com>
2019-09-03 14:36:53 +0200

    Remove SectionHeader from headings.js

--- src/components/Headings/Headings.js
+++ src/components/Headings/Headings.js
@@ -9,30 +9,6 @@ export const H3 = styled.h3``;
 
 export const H4 = styled.h4``;
 
-export const SectionHeader = styled.span`
-  color: ${theme.colors.grey700};
-  display: block;
-  text-transform: uppercase;
-  font-size: ${theme.font.size.body.subhead};
-  line-height: ${theme.font.lineHeight.body.subhead};
-  letter-spacing: ${theme.font.letterSpacing.body.subhead};
-
-  font-weight: 700;
-  font-style: normal;
-
-  font-family: ${theme.font.family.display.fallback};
-
-  .fonts-loaded & {
-    font-family: ${theme.font.family.display.bold};
-  }
-
-  ${mediaMin.s`
-    font-size: ${theme.font.size.body.subhead};
-    line-height: ${theme.font.lineHeight.body.subhead};
-    letter-spacing: ${theme.font.letterSpacing.body.subhead};
-  `}
-`;
-
 export const Subhead = styled.h4`
   color: ${theme.color.text.subdued};
   display: block;
