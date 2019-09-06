7ed389e0848046cdc3d6f70963e28b78b93f0c1a
Dani Lucaci <danilucaci@gmail.com>
2019-09-06 15:14:29 +0200

    Remove functions from helpers, prettier style changes

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,11 +1,12 @@
 import { COPY_CODE_MESSAGES, COPY_URL_MESSAGES } from "../i18n/i18n";
 
-let globalLocale = "en";
-
 export function calculateScroll() {
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  const clientHeight = window.innerHeight || document.documentElement.clientHeight;
-  const docScrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const clientHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const docScrollHeight =
+    document.body.scrollHeight || document.documentElement.scrollHeight;
   const docHeight = docScrollHeight - clientHeight;
   const scrolled = (winScroll / docHeight) * 100;
 
@@ -36,48 +37,15 @@ export function handleScrollLine() {
 }
 
 export function addSafariVideoControls() {
-  if (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1) {
+  if (
+    navigator.userAgent.indexOf("Safari") !== -1 &&
+    navigator.userAgent.indexOf("Chrome") === -1
+  ) {
     const videos = document.querySelectorAll("video");
     videos.forEach((video) => {
       video.controls = true;
     });
   }
-}
-
-// https://gist.github.com/matthagemann/382adfc57adbd5af078dc93feef01fe1
-export function slugify(string) {
-  const a = "àáäâãåèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;";
-  const b = "aaaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------";
-  const p = new RegExp(a.split("").join("|"), "g");
-
-  return string
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-    .replace(/\-\-+/g, "-") // Replace multiple - with single -
-    .replace(/^-+/, "") // Trim - from start of text
-    .replace(/-+$/, ""); // Trim - from end of text
-}
-
-/*!
- * Determine if an element is in the viewport
- * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
- * https://vanillajstoolkit.com/helpers/isinviewport/
- * @param  {Node}    element The element
- * @return {Boolean}      Returns true if element is in the viewport
- */
-export function isInViewport(element) {
-  const distance = element.getBoundingClientRect();
-
-  return (
-    distance.top >= 0 &&
-    distance.left >= 0 &&
-    distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
 }
 
 // Test via a getter in the options object to see if the passive property is accessed
@@ -95,25 +63,6 @@ export function textPassiveEventSupport() {
   } catch (e) {}
 
   return supportsPassive;
-}
-
-export function checkForDoNotTrack() {
-  // IE 10 or less has: window.external.msTrackingProtectionEnabled
-  // IE 9 and 10 have: navigator.msDoNotTrack.
-  // IE 11 and Edge have: window.doNotTrack.
-  // Gecko 32 Firefox has: navigator.doNotTrack returning yes or no, not 1 or 0.
-  // Safari 7.1.3+ has: window.doNotTrack.
-  const dnt = window.doNotTrack || navigator.msDoNotTrack || navigator.doNotTrack;
-  if (dnt !== "1" && dnt !== "yes") {
-    return false;
-  }
-  return true;
-}
-
-export function detectBrowserLanguage() {
-  return (
-    (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage
-  );
 }
 
 export function detectDataSaverMode() {
@@ -134,22 +83,24 @@ export function detectSlowConnectionType() {
   return false;
 }
 
-export function toUpperCamelCase(string) {
-  return string
-    .toLowerCase()
-    .replace(/(?:(^.)|(\s+.))/g, (match) => match.charAt(match.length - 1).toUpperCase());
-}
+/*!
+ * Determine if an element is in the viewport
+ * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * https://vanillajstoolkit.com/helpers/isinviewport/
+ * @param  {Node} element The element
+ * @return {Boolean} Returns true if element is in the viewport
+ */
+export function isInViewport(element) {
+  const distance = element.getBoundingClientRect();
 
-export function handleTOCScroll() {
-  const h2s = document.querySelectorAll("h2");
-
-  h2s.forEach((heading) => {
-    const isVis = isInViewport(heading);
-
-    if (isVis) {
-      // console.log("heading: ", heading);
-    }
-  });
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
 export function selectDummyNodeToCopy(dummyNode) {
