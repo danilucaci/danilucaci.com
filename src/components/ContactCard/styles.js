dd27ab4e59fd1be3a5419cb39016143fa04b2a73
Dani Lucaci <danilucaci@gmail.com>
2019-09-24 14:56:17 +0200

    Update contact card styles to have text centered

--- src/components/ContactCard/styles.js
+++ src/components/ContactCard/styles.js
@@ -4,44 +4,53 @@ import { theme, mediaMin, rem } from "../../theme/theme";
 import { Copy } from "../Copy/Copy";
 import { RowSpacer } from "../Grid/Grid";
 import SecondaryLinkButton from "../SecondaryLinkButton/SecondaryLinkButton";
-// import mobileBackground from "../../images/illustrations/contact_card_circles.svg";
 
 export const ContactCardRowBackground = styled(RowSpacer)`
   background-color: ${theme.color.background.section.contactCard};
-  /* background-image: url(${mobileBackground});
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-position: center center; */
 `;
 
 export const ContactCardTitle = styled.h2`
   color: ${theme.color.text.light.white};
-
   margin-bottom: ${rem(16)};
 
+  font-size: ${theme.font.size.display.mobile.contactCardTitle};
+  line-height: ${theme.font.lineHeight.display.mobile.contactCardTitle};
+
+  ${mediaMin.xs`
+    text-align: center;
+  `};
+
   ${mediaMin.m`
-    font-size: ${theme.font.size.display.desktop.h1};
-    line-height: ${theme.font.lineHeight.display.desktop.h1};
+    margin-bottom: ${rem(24)};
+    font-size: ${theme.font.size.display.desktop.contactCardTitle};
+    line-height: ${theme.font.lineHeight.display.desktop.contactCardTitle};
   `};
 `;
 
 export const Subtitle = styled(Copy)`
   color: ${theme.color.text.light.primary};
   display: block;
-  font-size: ${theme.font.size.display.mobile.subtitle};
-  line-height: ${theme.font.lineHeight.display.mobile.subtitle};
+  font-size: ${theme.font.size.display.mobile.contactCardSubtitle};
+  line-height: ${theme.font.lineHeight.display.mobile.contactCardSubtitle};
+  margin-bottom: ${rem(64)};
 
-  margin-bottom: ${rem(24)};
+  ${mediaMin.xs`
+    text-align: center;
+  `};
 
-  ${mediaMin.s`
-    margin-bottom: ${rem(48)};
-    font-size: ${theme.font.size.display.desktop.subtitle};
-    line-height: ${theme.font.lineHeight.display.desktop.subtitle};
+  ${mediaMin.m`
+    margin-bottom: ${rem(40)};
+    font-size: ${theme.font.size.display.desktop.contactCardSubtitle};
+    line-height: ${theme.font.lineHeight.display.desktop.contactCardSubtitle};
   `};
 `;
 
 export const StyledButton = styled(SecondaryLinkButton)`
-  ${mediaMin.xs`
-    width: ${rem(240)};  
+  width: 100%;
+
+  ${mediaMin.s`
+    width: ${rem(340)};
+    margin-left: auto;
+    margin-right: auto;
   `};
 `;
