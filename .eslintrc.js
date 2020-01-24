fd85d8381b976502f8acd7eca009582c31bd15cd
Dani Lucaci <danilucaci@gmail.com>
2020-01-24 10:57:52 +0100

    Add .eslintrc babel-eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@
 // "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
 // "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
 module.exports = {
+  parser: "babel-eslint",
   extends: [
     "plugin:prettier/recommended",
     "prettier/react",
