1abd08103538e4eb9c83de83e8eba5f96e38ab4e
Dani Lucaci <danilucaci@gmail.com>
2020-02-05 16:06:16 +0100

    Update about page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { shape, object, string } from "prop-types";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
-import { FormattedMessage } from "react-intl";
+import { useIntl } from "react-intl";
 
 import SEO from "../components/SEO/SEO";
 import Layout from "../components/Layout";
@@ -17,126 +17,163 @@ import { localePaths } from "../i18n/i18n";
 import {
   HeaderRow,
   HeaderInfoCol,
-  ResumeWrapper,
   AboutMeTitle,
   AboutCopy,
   StyledCopy,
   AltRowBackground,
-  DoingNowTitle,
-  DoingNowProjectsRow,
-  DoingNowSubTitle,
+  MyWorkTitle,
+  SkillsTitle,
+  SkillsSubtitle,
+  SkilsCol,
 } from "../styles/about-me.styles";
 import LocaleContext from "../i18n/LocaleContext";
 
 const AboutPage = ({ location, data }) => {
   const { locale } = useContext(LocaleContext);
-
-  let twinPostURL = "";
-
-  if (locale === "en") {
-    twinPostURL = localePaths["es"].about;
-  } else if (locale === "es") {
-    twinPostURL = localePaths["en"].about;
-  }
+  const intl = useIntl();
 
   return (
     <ErrorBoundary>
       <Layout
         location={location}
-        twinPostURL={twinPostURL}
         expandHeaderAndFooter
+        twinPostURL={
+          locale === "en" ? localePaths["es"].about : localePaths["en"].about
+        }
       >
         <SEO
-          twinPostURL={twinPostURL}
           currentPage="about"
           currentPath={location.pathname}
+          twinPostURL={
+            locale === "en" ? localePaths["es"].about : localePaths["en"].about
+          }
         />
-
         <Main>
           <HeaderRow as="header" col12 mb>
-            <Col s={4}>
-              <FormattedMessage id="about.the.blog.image.title">
-                {(txt) => (
-                  <Img
-                    alt={txt}
-                    fluid={data.aboutImage.childImageSharp.fluid}
-                  />
-                )}
-              </FormattedMessage>
-              <ResumeWrapper>
-                <SocialNav />
-              </ResumeWrapper>
+            <Col xl={5}>
+              <Img
+                alt={intl.formatMessage({ id: "about.me.image.alt" })}
+                fluid={data.aboutImage.childImageSharp.fluid}
+              />
             </Col>
-            <HeaderInfoCol s={8}>
-              <FormattedMessage id="about.me.title">
-                {(txt) => <AboutMeTitle>{txt}</AboutMeTitle>}
-              </FormattedMessage>
-              <FormattedMessage id="about.me.copy.1">
-                {(txt) => <AboutCopy>{txt}</AboutCopy>}
-              </FormattedMessage>
-              <FormattedMessage id="about.me.copy.2">
-                {(txt) => <AboutCopy>{txt}</AboutCopy>}
-              </FormattedMessage>
-              <FormattedMessage id="about.me.copy.3">
-                {(txt) => <AboutCopy>{txt}</AboutCopy>}
-              </FormattedMessage>
+            <HeaderInfoCol xl={7}>
+              <AboutMeTitle>
+                {intl.formatMessage({ id: "about.me.title" })}
+              </AboutMeTitle>
+              <AboutCopy>
+                {intl.formatMessage({ id: "about.me.subtitle" })}
+              </AboutCopy>
+              <SocialNav />
             </HeaderInfoCol>
           </HeaderRow>
           <AltRowBackground padded>
-            <Row col12 as="div">
-              <Col xxl={6}>
-                <FormattedMessage id="about.me.what.now.title">
-                  {(txt) => <DoingNowTitle>{txt}</DoingNowTitle>}
-                </FormattedMessage>
-                <FormattedMessage id="about.me.what.now.copy.1">
-                  {(txt) => <StyledCopy>{txt}</StyledCopy>}
-                </FormattedMessage>
-                <FormattedMessage id="about.me.what.now.copy.2">
-                  {(txt) => <StyledCopy>{txt}</StyledCopy>}
-                </FormattedMessage>
+            <Row col12 pb as="div">
+              <Col xxl={5}>
+                <MyWorkTitle>
+                  {intl.formatMessage({ id: "about.me.what.i.do.title" })}
+                </MyWorkTitle>
+              </Col>
+              <Col xxl={7}>
+                <StyledCopy>
+                  {intl.formatMessage({ id: "about.me.what.i.do.copy.1" })}
+                </StyledCopy>
+                <StyledCopy>
+                  {intl.formatMessage({ id: "about.me.what.i.do.copy.2" })}
+                </StyledCopy>
+                <StyledCopy>
+                  {intl.formatMessage({ id: "about.me.what.i.do.copy.3" })}
+                </StyledCopy>
+                <StyledCopy>
+                  {intl.formatMessage({ id: "about.me.what.i.do.copy.4" })}
+                </StyledCopy>
               </Col>
             </Row>
-            <DoingNowProjectsRow col12 as="div">
-              <Col l={6} xl={4}>
-                <FormattedMessage id="about.me.what.now.skills">
-                  {(txt) => <DoingNowSubTitle>{txt}</DoingNowSubTitle>}
-                </FormattedMessage>
-                <ul>
-                  <FormattedMessage id="about.me.what.now.skills.1">
-                    {(txt) => <li>{txt}</li>}
-                  </FormattedMessage>
-                  <FormattedMessage id="about.me.what.now.skills.2">
-                    {(txt) => <li>{txt}</li>}
-                  </FormattedMessage>
-                  <FormattedMessage id="about.me.what.now.skills.3">
-                    {(txt) => <li>{txt}</li>}
-                  </FormattedMessage>
-                </ul>
+            <Row col12 as="div">
+              <Col col={12}>
+                <SkillsTitle>
+                  {intl.formatMessage({ id: "about.me.skills.title" })}
+                </SkillsTitle>
               </Col>
-              <Col l={6} xl={4}>
-                <FormattedMessage id="about.me.what.now.projects">
-                  {(txt) => <DoingNowSubTitle>{txt}</DoingNowSubTitle>}
-                </FormattedMessage>
+              <SkilsCol xxs={6} s={4} xl={3}>
+                <SkillsSubtitle>
+                  {intl.formatMessage({ id: "about.me.skills.1.subtitle" })}
+                </SkillsSubtitle>
                 <ul>
-                  <FormattedMessage id="about.me.what.now.projects.1">
-                    {(txt) => <li>{txt}</li>}
-                  </FormattedMessage>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.1.item.1" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.1.item.2" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.1.item.3" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.1.item.4" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.1.item.5" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.1.item.6" })}
+                  </li>
                 </ul>
-              </Col>
-              <Col l={6} xl={4}>
-                <FormattedMessage id="about.me.what.now.books">
-                  {(txt) => <DoingNowSubTitle>{txt}</DoingNowSubTitle>}
-                </FormattedMessage>
+              </SkilsCol>
+              <SkilsCol xxs={6} s={4} xl={3}>
+                <SkillsSubtitle>
+                  {intl.formatMessage({ id: "about.me.skills.2.subtitle" })}
+                </SkillsSubtitle>
                 <ul>
-                  <FormattedMessage id="about.me.what.now.books.1">
-                    {(txt) => <li>{txt}</li>}
-                  </FormattedMessage>
-                  <FormattedMessage id="about.me.what.now.books.2">
-                    {(txt) => <li>{txt}</li>}
-                  </FormattedMessage>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.2.item.1" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.2.item.2" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.2.item.3" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.2.item.4" })}
+                  </li>
                 </ul>
-              </Col>
-            </DoingNowProjectsRow>
+              </SkilsCol>
+              <SkilsCol xxs={6} s={4} xl={3}>
+                <SkillsSubtitle>
+                  {intl.formatMessage({ id: "about.me.skills.3.subtitle" })}
+                </SkillsSubtitle>
+                <ul>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.3.item.1" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.3.item.2" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.3.item.3" })}
+                  </li>
+                </ul>
+              </SkilsCol>
+              <SkilsCol xxs={6} s={4} xl={3}>
+                <SkillsSubtitle>
+                  {intl.formatMessage({ id: "about.me.skills.4.subtitle" })}
+                </SkillsSubtitle>
+                <ul>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.4.item.1" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.4.item.2" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.4.item.3" })}
+                  </li>
+                  <li>
+                    {intl.formatMessage({ id: "about.me.skills.4.item.4" })}
+                  </li>
+                </ul>
+              </SkilsCol>
+            </Row>
           </AltRowBackground>
         </Main>
         <ContactCard />
@@ -162,7 +199,7 @@ export const query = graphql`
   query ABOUT_ME_IMAGE_QUERY {
     aboutImage: file(relativePath: { regex: "/danilucaci_profile_image/" }) {
       childImageSharp {
-        fluid(maxWidth: 560, maxHeight: 640, cropFocus: NORTH, quality: 70) {
+        fluid(maxWidth: 720, maxHeight: 540, cropFocus: NORTH, quality: 70) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
--- src/styles/about-me.styles.js
+++ src/styles/about-me.styles.js
@@ -1,22 +1,23 @@
 import styled from "styled-components";
-import { theme, mediaMin, rem } from "../theme/theme";
+import { theme, mediaMin, mediaMax, rem } from "../theme/theme";
 import { Copy } from "../components/Copy/Copy";
 import { Row, RowSpacer, Col } from "../components/Grid/Grid";
 
 export const HeaderRow = styled(Row)`
-  ${mediaMin.l`
+  ${mediaMin.xl`
     margin-top: ${rem(40)};
+    align-items: center;
   `};
 
   ${mediaMin.xxl`
-    align-items: center;
+    margin-top: ${rem(64)};
   `};
 `;
 
 export const HeaderInfoCol = styled(Col)`
-  margin-top: ${rem(40)};
+  margin-top: ${rem(16)};
 
-  ${mediaMin.s`
+  ${mediaMin.xl`
     margin-top: 0;
   `};
 
@@ -40,27 +41,25 @@ export const AboutMeTitle = styled.h1`
 
 export const AboutCopy = styled(Copy)`
   margin-bottom: ${rem(16)};
+
+  ${mediaMin.s`
+    margin-bottom: ${rem(24)};
+  `}
 `;
 
 export const StyledCopy = styled(Copy)`
   margin-bottom: ${rem(32)};
 
   &:last-of-type {
-    margin-bottom: ${rem(20)};
+    margin-bottom: 0;
   }
-`;
-
-export const ResumeWrapper = styled.div`
-  display: block;
-  margin-top: ${rem(16)};
-  width: 100%;
 `;
 
 export const AltRowBackground = styled(RowSpacer)`
   background-color: ${theme.color.background.section.lightest};
 `;
 
-export const DoingNowTitle = styled.h2`
+export const MyWorkTitle = styled.h2`
   margin-bottom: ${rem(16)};
 
   ${mediaMin.s`
@@ -68,31 +67,28 @@ export const DoingNowTitle = styled.h2`
   `};
 `;
 
-export const DoingNowSubTitle = styled.h3`
-  font-size: ${theme.font.size.display.mobile.h5};
-  line-height: ${theme.font.lineHeight.display.mobile.h5};
-
-  ${mediaMin.xs`
-    font-size: ${theme.font.size.display.desktop.h5};
-    line-height: ${theme.font.lineHeight.display.desktop.h5};
-  `}
-
-  margin-top: ${rem(32)};
+export const SkillsTitle = styled.h2`
   margin-bottom: ${rem(16)};
 
-  &:first-child {
-    margin-top: ${rem(16)};
-  }
-
-  ${mediaMin.s`
-    margin-top: 0;
+  ${mediaMin.xl`
+    margin-bottom: ${rem(40)};
   `};
 `;
 
-export const DoingNowProjectsRow = styled(Row)`
-  margin-top: ${rem(16)};
+export const SkillsSubtitle = styled.h3`
+  font-size: ${theme.font.size.display.mobile.h4};
+  line-height: ${theme.font.lineHeight.display.mobile.h4};
 
-  ${mediaMin.s`
-    margin-top: ${rem(32)};
+  ${mediaMin.xs`
+    font-size: ${theme.font.size.display.desktop.h4};
+    line-height: ${theme.font.lineHeight.display.desktop.h4};
+  `}
+
+  margin-bottom: ${rem(16)};
+`;
+
+export const SkilsCol = styled(Col)`
+  ${mediaMax.s`
+    margin-bottom: ${rem(40)};
   `};
 `;
