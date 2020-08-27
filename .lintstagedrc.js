9460f9092461cf5664d252e7cc05e010f56c4424
Dani Lucaci <danilucaci@gmail.com>
2020-08-27 09:43:56 +0200

    Add lint:js:fix script in lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "src/**/*.js": ["npm run lint:js", "npm run test:related"],
+  "src/**/*.js": ["npm run lint:js:fix", "npm run test:related"],
   "*.{md,mdx,css}": ["npm run lint:prettier:write"],
 };
