040ce85239e3e4747b34e4bbd57b268a34cb669a
Dani Lucaci <danilucaci@gmail.com>
2020-08-27 17:11:08 +0200

    Use localePaths instead of twinPostURL in ContactCard

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -4,6 +4,7 @@ import { useIntl } from "react-intl";
 import { Col, Row } from "../Grid/Grid";
 import { sendContactCardEvent } from "../../helpers/ga";
 import LocaleContext from "../../i18n/LocaleContext";
+import localePaths from "../../i18n/localePaths";
 
 import {
   ContactCardRowBackground,
@@ -16,14 +17,6 @@ function ContactCard() {
   const { locale } = useContext(LocaleContext);
   const intl = useIntl();
 
-  let twinPostURL = "";
-
-  if (locale === "en") {
-    twinPostURL = "/contact";
-  } else if (locale === "es") {
-    twinPostURL = "/es/contacto";
-  }
-
   return (
     <ContactCardRowBackground as="aside" data-testid="Contact__Card">
       <Row as="div" col8 padded>
@@ -32,7 +25,10 @@ function ContactCard() {
             {intl.formatMessage({ id: "contact.card.title" })}
           </ContactCardTitle>
           <Subtitle>{intl.formatMessage({ id: "contact.card.info" })}</Subtitle>
-          <StyledButton to={twinPostURL} onClick={() => sendContactCardEvent()}>
+          <StyledButton
+            to={localePaths[locale].contact}
+            onClick={() => sendContactCardEvent()}
+          >
             {intl.formatMessage({ id: "contact.card.cta" })}
           </StyledButton>
         </Col>
