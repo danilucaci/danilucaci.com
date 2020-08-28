9ff70bfdf7eecbbc3f349e8369ac2fe3d88ba212
Dani Lucaci <danilucaci@gmail.com>
2020-08-28 20:24:04 +0200

    Add missing prop types in CookiesProvider

--- src/context/CookiesContext.js
+++ src/context/CookiesContext.js
@@ -1,4 +1,5 @@
 import React, { createContext } from "react";
+import { node, shape, string, object } from "prop-types";
 import { useCookies } from "../hooks";
 import cookiesInitialState from "../reducers/cookies/cookies-initial-state";
 
@@ -24,5 +25,29 @@ function CookiesProvider({ children, location }) {
     </CookiesContext.Provider>
   );
 }
+
+CookiesProvider.propTypes = {
+  children: node.isRequired,
+  location: shape({
+    hash: string.isRequired,
+    host: string.isRequired,
+    hostname: string.isRequired,
+    href: string.isRequired,
+    key: string.isRequired,
+    origin: string.isRequired,
+    pathname: string.isRequired,
+    port: string.isRequired,
+    protocol: string.isRequired,
+    search: string.isRequired,
+    // eslint-disable-next-line react/forbid-prop-types
+    state: object,
+  }),
+};
+
+CookiesProvider.defaultProps = {
+  location: {
+    state: null,
+  },
+};
 
 export default CookiesProvider;
