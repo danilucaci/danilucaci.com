0b13ad7c0295b6ebbf591bdf8dc1a8b4d9bdb5d2
Dani Lucaci <danilucaci@gmail.com>
2020-08-28 19:51:08 +0200

    Add missing prop types in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { node } from "prop-types";
 
 import { StyledMain } from "./styles";
 
 function Main({ children }) {
   return <StyledMain role="main">{children}</StyledMain>;
 }
+
+Main.propTypes = {
+  children: node.isRequired,
+};
 
 export default Main;
