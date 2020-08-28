24e4cd165de6e53e240b65b84917efc73d883d8f
Dani Lucaci <danilucaci@gmail.com>
2020-08-28 20:09:13 +0200

    Add missing prop types in SocialNav

--- src/components/SocialNav/SocialNav.js
+++ src/components/SocialNav/SocialNav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useIntl } from "react-intl";
+import { string, func, bool } from "prop-types";
 
 import { Wrapper, StyledIcon, StyledLink } from "./styles";
 import { sendSocialNavVisitEvent } from "../../helpers/ga";
@@ -36,6 +37,18 @@ export function SocialNavProfileLink({
   );
 }
 
+SocialNavProfileLink.propTypes = {
+  socialMediaName: string.isRequired,
+  socialMediaLink: string.isRequired,
+  socialMediaID: string.isRequired,
+  handleClick: func.isRequired,
+  light: bool,
+};
+
+SocialNavProfileLink.defaultProps = {
+  light: false,
+};
+
 function SocialNav({ light }) {
   return (
     <Wrapper>
@@ -52,5 +65,13 @@ function SocialNav({ light }) {
     </Wrapper>
   );
 }
+
+SocialNav.propTypes = {
+  light: bool,
+};
+
+SocialNav.defaultProps = {
+  light: false,
+};
 
 export default SocialNav;
