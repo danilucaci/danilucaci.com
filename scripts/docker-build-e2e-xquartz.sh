c630cd3811e104cfd2c23f7892798e910a5bce16
Dani Lucaci <danilucaci@gmail.com>
2021-03-06 21:30:55 +0100

    build: add dynamic ip in sh docker scripts

--- scripts/docker-build-e2e-xquartz.sh
+++ scripts/docker-build-e2e-xquartz.sh
@@ -1,9 +1,15 @@
 #! /usr/bin/env bash
 
+# XQuartz must be running first:
+open -a XQuartz
+
+# add your host ip to the allowed hosts
+/usr/X11/bin/xhost + $(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
 docker-compose \
   -f docker-compose.yml \
   -f docker-compose-cypress.yml \
   -f docker-compose-cypress-xquartx.yml \
   up \
-  --build \
-  --exit-code-from cypress
+  --build
+# --exit-code-from cypress
