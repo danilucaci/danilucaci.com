fd9012b072fb8e2caec897372cc0b208cd6603ef
Dani Lucaci <danilucaci@gmail.com>
2021-03-06 17:58:05 +0100

    build: refactor lint-staged file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,20 +1,18 @@
 const path = require("path");
 
-const WEB_PACKAGE = "web";
-const API_PACKAGE = "api";
+const WEB_PATH = "web";
+const API_PATH = "api";
 
 const LINT_JS_CMD = "lint:js";
 const LINT_FORMAT_CHECK_CMD = "lint:format:check";
 const TEST_RELATED_CMD = "test:related";
 
 function makeDockerComposeExecCommand(
-  packageName = WEB_PACKAGE,
+  packageName = WEB_PATH,
   script = TEST_RELATED_CMD,
 ) {
   return function dockerComposeExecCommandThunk(absolutePaths) {
     const cwd = process.cwd();
-
-    console.log(absolutePaths);
 
     const relativePaths = absolutePaths
       .map((file) =>
@@ -29,28 +27,28 @@ function makeDockerComposeExecCommand(
 }
 
 module.exports = {
-  "packages/web/src/**/*.js": makeDockerComposeExecCommand(
-    WEB_PACKAGE,
+  [`packages/${WEB_PATH}/src/**/*.js`]: makeDockerComposeExecCommand(
+    WEB_PATH,
     TEST_RELATED_CMD,
   ),
-  "packages/api/src/**/*.js": makeDockerComposeExecCommand(
-    API_PACKAGE,
+  [`packages/${API_PATH}/src/**/*.js`]: makeDockerComposeExecCommand(
+    API_PATH,
     TEST_RELATED_CMD,
   ),
-  "packages/web/**/*.js": makeDockerComposeExecCommand(
-    WEB_PACKAGE,
+  [`packages/${WEB_PATH}/**/*.js`]: makeDockerComposeExecCommand(
+    WEB_PATH,
     LINT_JS_CMD,
   ),
-  "packages/api/**/*.js": makeDockerComposeExecCommand(
-    API_PACKAGE,
+  [`packages/${API_PATH}/**/*.js`]: makeDockerComposeExecCommand(
+    API_PATH,
     LINT_JS_CMD,
   ),
-  "packages/web/**/*.{js,json,css,yml,yaml,toml,md,mdx}": makeDockerComposeExecCommand(
-    WEB_PACKAGE,
+  [`packages/${WEB_PATH}/**/*.{js,json,css,yml,yaml,toml,md,mdx}`]: makeDockerComposeExecCommand(
+    WEB_PATH,
     LINT_FORMAT_CHECK_CMD,
   ),
-  "packages/api/**/*.{js,json,yml,yaml,md}": makeDockerComposeExecCommand(
-    API_PACKAGE,
+  [`packages/${API_PATH}/**/*.{js,json,yml,yaml,md}`]: makeDockerComposeExecCommand(
+    API_PATH,
     LINT_FORMAT_CHECK_CMD,
   ),
 };
