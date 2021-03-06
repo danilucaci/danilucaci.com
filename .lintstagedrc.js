27ae5d990b699aa94f23e053ff20e257ca5e0e49
Dani Lucaci <danilucaci@gmail.com>
2021-03-06 17:53:21 +0100

    build: separate the lint-staged rules per package

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,56 @@
+const path = require("path");
+
+const WEB_PACKAGE = "web";
+const API_PACKAGE = "api";
+
+const LINT_JS_CMD = "lint:js";
+const LINT_FORMAT_CHECK_CMD = "lint:format:check";
+const TEST_RELATED_CMD = "test:related";
+
+function makeDockerComposeExecCommand(
+  packageName = WEB_PACKAGE,
+  script = TEST_RELATED_CMD,
+) {
+  return function dockerComposeExecCommandThunk(absolutePaths) {
+    const cwd = process.cwd();
+
+    console.log(absolutePaths);
+
+    const relativePaths = absolutePaths
+      .map((file) =>
+        path.relative(cwd, file).replace(`packages/${packageName}/`, ""),
+      )
+      .join(" ");
+
+    console.log(relativePaths);
+
+    return `docker-compose exec -T ${packageName} yarn run ${script} ${relativePaths}`;
+  };
+}
+
 module.exports = {
-  "packages/*/src/**/*.js": [
-    "docker-compose exec -T web yarn run test:related",
-    "docker-compose exec -T api yarn run test:related",
-  ],
-  "*.js": [
-    "docker-compose exec -T web yarn run lint:js",
-    "docker-compose exec -T api yarn run lint:js",
-  ],
-  "*.{js,json,css,yml,yaml,toml,md,mdx}": [
-    "docker-compose exec -T web yarn run lint:format:check",
-    "docker-compose exec -T api yarn run lint:format:check",
-  ],
+  "packages/web/src/**/*.js": makeDockerComposeExecCommand(
+    WEB_PACKAGE,
+    TEST_RELATED_CMD,
+  ),
+  "packages/api/src/**/*.js": makeDockerComposeExecCommand(
+    API_PACKAGE,
+    TEST_RELATED_CMD,
+  ),
+  "packages/web/**/*.js": makeDockerComposeExecCommand(
+    WEB_PACKAGE,
+    LINT_JS_CMD,
+  ),
+  "packages/api/**/*.js": makeDockerComposeExecCommand(
+    API_PACKAGE,
+    LINT_JS_CMD,
+  ),
+  "packages/web/**/*.{js,json,css,yml,yaml,toml,md,mdx}": makeDockerComposeExecCommand(
+    WEB_PACKAGE,
+    LINT_FORMAT_CHECK_CMD,
+  ),
+  "packages/api/**/*.{js,json,yml,yaml,md}": makeDockerComposeExecCommand(
+    API_PACKAGE,
+    LINT_FORMAT_CHECK_CMD,
+  ),
 };
