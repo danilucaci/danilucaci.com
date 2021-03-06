bd35866c0b3be22b068b9ef5bdf900d522262d61
Dani Lucaci <danilucaci@gmail.com>
2021-03-06 18:23:11 +0100

    build: modularize lint-staged cb

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,48 +7,75 @@ const LINT_JS_CMD = "lint:js";
 const LINT_FORMAT_CHECK_CMD = "lint:format:check";
 const TEST_RELATED_CMD = "test:related";
 
-function makeDockerComposeExecCommand(
-  packageName = WEB_PATH,
-  script = TEST_RELATED_CMD,
-) {
-  return function dockerComposeExecCommandThunk(absolutePaths) {
+const DOCKER_CMD = "docker-compose exec -T";
+const NPM_CLIENT = "yarn";
+const PACKAGES_PATH = "packages";
+
+function makeExecCommand({
+  packagesPath = "packages",
+  appPath = "web",
+  dockerCmd = "docker-compose exec -T",
+  npmClient = "yarn",
+  script = "test:related",
+} = {}) {
+  return function lintStagedThunk(absolutePaths) {
     const cwd = process.cwd();
 
     const relativePaths = absolutePaths
       .map((file) =>
-        path.relative(cwd, file).replace(`packages/${packageName}/`, ""),
+        path.relative(cwd, file).replace(`${packagesPath}/${appPath}/`, ""),
       )
       .join(" ");
 
-    console.log(relativePaths);
+    console.log({ relativePaths: relativePaths });
 
-    return `docker-compose exec -T ${packageName} yarn run ${script} ${relativePaths}`;
+    return `${dockerCmd} ${appPath} ${npmClient} run ${script} ${relativePaths}`;
   };
 }
 
 module.exports = {
-  [`packages/${WEB_PATH}/src/**/*.js`]: makeDockerComposeExecCommand(
-    WEB_PATH,
-    TEST_RELATED_CMD,
+  [`${PACKAGES_PATH}/${WEB_PATH}/src/**/*.js`]: makeExecCommand({
+    packagesPath: PACKAGES_PATH,
+    appPath: WEB_PATH,
+    dockerCmd: DOCKER_CMD,
+    npmClient: NPM_CLIENT,
+    script: TEST_RELATED_CMD,
+  }),
+  [`${PACKAGES_PATH}/${API_PATH}/src/**/*.js`]: makeExecCommand({
+    packagesPath: PACKAGES_PATH,
+    appPath: API_PATH,
+    dockerCmd: DOCKER_CMD,
+    npmClient: NPM_CLIENT,
+    script: TEST_RELATED_CMD,
+  }),
+  [`${PACKAGES_PATH}/${WEB_PATH}/**/*.js`]: makeExecCommand({
+    packagesPath: PACKAGES_PATH,
+    appPath: WEB_PATH,
+    dockerCmd: DOCKER_CMD,
+    npmClient: NPM_CLIENT,
+    script: LINT_JS_CMD,
+  }),
+  [`${PACKAGES_PATH}/${API_PATH}/**/*.js`]: makeExecCommand({
+    packagesPath: PACKAGES_PATH,
+    appPath: API_PATH,
+    dockerCmd: DOCKER_CMD,
+    npmClient: NPM_CLIENT,
+    script: LINT_JS_CMD,
+  }),
+  [`${PACKAGES_PATH}/${WEB_PATH}/**/*.{js,json,css,yml,yaml,toml,md,mdx}`]: makeExecCommand(
+    {
+      packagesPath: PACKAGES_PATH,
+      appPath: WEB_PATH,
+      dockerCmd: DOCKER_CMD,
+      npmClient: NPM_CLIENT,
+      script: LINT_FORMAT_CHECK_CMD,
+    },
   ),
-  [`packages/${API_PATH}/src/**/*.js`]: makeDockerComposeExecCommand(
-    API_PATH,
-    TEST_RELATED_CMD,
-  ),
-  [`packages/${WEB_PATH}/**/*.js`]: makeDockerComposeExecCommand(
-    WEB_PATH,
-    LINT_JS_CMD,
-  ),
-  [`packages/${API_PATH}/**/*.js`]: makeDockerComposeExecCommand(
-    API_PATH,
-    LINT_JS_CMD,
-  ),
-  [`packages/${WEB_PATH}/**/*.{js,json,css,yml,yaml,toml,md,mdx}`]: makeDockerComposeExecCommand(
-    WEB_PATH,
-    LINT_FORMAT_CHECK_CMD,
-  ),
-  [`packages/${API_PATH}/**/*.{js,json,yml,yaml,md}`]: makeDockerComposeExecCommand(
-    API_PATH,
-    LINT_FORMAT_CHECK_CMD,
-  ),
+  [`${PACKAGES_PATH}/${API_PATH}/**/*.{js,json,yml,yaml,md}`]: makeExecCommand({
+    packagesPath: PACKAGES_PATH,
+    appPath: API_PATH,
+    dockerCmd: DOCKER_CMD,
+    npmClient: NPM_CLIENT,
+    script: LINT_FORMAT_CHECK_CMD,
+  }),
 };
