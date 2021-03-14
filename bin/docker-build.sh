49b40d4d035cfea38b46e19849014bb20ebf3966
Dani Lucaci <danilucaci@gmail.com>
2021-03-14 18:28:00 +0100

    build: add working cypress yarn integration

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,6 +1,18 @@
 #! /usr/bin/env bash
 
+# build the latest version of the image
+docker build -t danilucaci/app:latest . -f Dockerfile.dev
+
+# XQuartz must be running first:
+open -a XQuartz
+
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+# add your host ip to the allowed hosts
+/usr/X11/bin/xhost + $IP
+
 docker-compose \
   -f docker-compose.yml \
+  -f docker-compose-e2e.yml \
   up \
   --build
