2edc48fc3cf7913921972d5b0e26313d923e4149
Dani Lucaci <danilucaci@gmail.com>
2021-04-01 14:12:11 +0200

    feat: add lintstaged paths logging

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
 
 # installs global packages needed for git hooks
-yarn global install @commitlint/cli @commitlint/config-conventional commitlint husky lint-staged
+yarn global add @commitlint/cli@12.0.1 \
+  @commitlint/config-conventional@12.0.1 \
+  commitlint@12.0.1 \
+  husky@6.0.0 \
+  lint-staged@10.5.4 \
+  chalk@4.1.0
