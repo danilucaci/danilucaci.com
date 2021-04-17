a45f86239254fd07fafadc14dc293128aae5ede1
Dani Lucaci <danilucaci@gmail.com>
2021-04-17 09:06:25 +0200

    chore: remove console.log from connect.js

--- packages/api/src/db/connect.js
+++ packages/api/src/db/connect.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const { config } = require("../config");
 
 function connect() {
-  console.log(config.db.url);
-
   return mongoose.connect(config.db.url, {
     useCreateIndex: true,
     useNewUrlParser: true,
