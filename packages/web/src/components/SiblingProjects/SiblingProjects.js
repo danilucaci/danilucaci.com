2a57a03e048108a88ca359d182ddcc39e8fe49e0
Dani Lucaci <danilucaci@gmail.com>
2021-05-08 16:45:00 +0200

    refactor: switch to useIntl in SiblingProjects

--- packages/web/src/components/SiblingProjects/SiblingProjects.js
+++ packages/web/src/components/SiblingProjects/SiblingProjects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { string } from "prop-types";
-import { FormattedMessage } from "react-intl";
+import { useIntl } from "react-intl";
 import HR from "../HR";
 import { Col, Row } from "../Grid/Grid";
 
@@ -14,32 +14,36 @@ import {
   NextArticleLink,
 } from "./styles";
 
-const SiblingProjects = ({ prevSlug, prevTitle, nextSlug, nextTitle }) => (
-  <Row pb col12>
-    <Col>
-      <HR />
-      <SiblingProjectsContents>
-        {prevSlug && (
-          <PreviousItemsContainer>
-            <FormattedMessage id="projects.prev">
-              {(txt) => <PrevSectionHeader>{txt}</PrevSectionHeader>}
-            </FormattedMessage>
-            <PrevArticleLink to={prevSlug}>{prevTitle}</PrevArticleLink>
-          </PreviousItemsContainer>
-        )}
-        {nextSlug && (
-          <NextItemsContainer>
-            <FormattedMessage id="projects.next">
-              {(txt) => <NextSectionHeader>{txt}</NextSectionHeader>}
-            </FormattedMessage>
-            <NextArticleLink to={nextSlug}>{nextTitle}</NextArticleLink>
-          </NextItemsContainer>
-        )}
-      </SiblingProjectsContents>
-      <HR />
-    </Col>
-  </Row>
-);
+function SiblingProjects({ prevSlug, prevTitle, nextSlug, nextTitle }) {
+  const intl = useIntl();
+
+  return (
+    <Row pb col12>
+      <Col>
+        <HR />
+        <SiblingProjectsContents>
+          {prevSlug && (
+            <PreviousItemsContainer>
+              <PrevSectionHeader>
+                {intl.formatMessage({ id: "projects.prev" })}
+              </PrevSectionHeader>
+              <PrevArticleLink to={prevSlug}>{prevTitle}</PrevArticleLink>
+            </PreviousItemsContainer>
+          )}
+          {nextSlug && (
+            <NextItemsContainer>
+              <NextSectionHeader>
+                {intl.formatMessage({ id: "projects.next" })}
+              </NextSectionHeader>
+              <NextArticleLink to={nextSlug}>{nextTitle}</NextArticleLink>
+            </NextItemsContainer>
+          )}
+        </SiblingProjectsContents>
+        <HR />
+      </Col>
+    </Row>
+  );
+}
 
 SiblingProjects.propTypes = {
   nextTitle: string,
