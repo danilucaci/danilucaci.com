32a052ce94ceba1bf45564bb50c851e6d57e8fa1
Dani Lucaci <danilucaci@gmail.com>
2021-05-08 16:20:51 +0200

    refactor: switch to useIntl in ExternalLinkIcon

--- packages/web/src/components/ExternalLinkIcon/ExternalLinkIcon.js
+++ packages/web/src/components/ExternalLinkIcon/ExternalLinkIcon.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { useIntl } from "react-intl";
 
 import { StyledExternalLinkIcon } from "./styles";
 import AriaText from "../AriaText";
 
-const ExternalLinkIcon = () => (
-  <StyledExternalLinkIcon>
-    <FormattedMessage id="external.link.sr.only">
-      {(txt) => <AriaText>{txt}</AriaText>}
-    </FormattedMessage>
-    <span aria-hidden="true" className="external-link" />
-  </StyledExternalLinkIcon>
-);
+function ExternalLinkIcon() {
+  const intl = useIntl();
+
+  return (
+    <StyledExternalLinkIcon>
+      <AriaText>{intl.formatMessage({ id: "external.link.sr.only" })}</AriaText>
+      <span aria-hidden="true" className="external-link" />
+    </StyledExternalLinkIcon>
+  );
+}
 
 export default ExternalLinkIcon;
